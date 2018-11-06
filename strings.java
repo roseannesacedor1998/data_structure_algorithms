b488b3014ebde119413cca4b94545990212c7674
roseannesacedor1998 <44794422+roseannesacedor1998@users.noreply.github.com>
2018-11-06 13:11:03 +0800

    Add files via upload

---
+++ strings.java
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="utf-8"?>
+<resources>
+
+    <string name="app_name">Quiz App</string> 
+    <string name="home_message"> Pick Quiz difficulty below?</string> 
+    <string name="action_settings">Settings</string> 
+	<string name="normal_quiz">Normal Quiz</string> 
+	<string name="hard_quiz">Hard Quiz</string> 
+	<string name="yes">True</string>
+	<string name="no">False</string> 
+	<string name="next">NEXT</string>
+	<string name="main_question">Your score are?</string> 
+	
+	
+</resources>
